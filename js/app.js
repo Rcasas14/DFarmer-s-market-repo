d0bd26e8da1dd28a8b172ecabb033293053354bf
Reymart Casas <rcasas@axxess.com>
2022-06-26 13:38:32 +0800

    added timline and design

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ const headerSideNav = document.querySelector('.header__side_nav');
 const hero = document.querySelector('.hero');
 
 
-btnHamburger.addEventListener('click', ()=>{
-    if(header.classList.contains('open-menu')){
+
+btnHamburger.addEventListener('click', () => {
+    if (header.classList.contains('open-menu')) {
         header.classList.remove('open-menu');
         headerSideNav.classList.add('d-none');
         headerSideNav.classList.add('side-fade-out');
@@ -18,4 +19,25 @@ btnHamburger.addEventListener('click', ()=>{
         headerSideNav.classList.add('side-fade-in');
         hero.classList.add('opacity');
     }
-})
+});
+
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+  
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
